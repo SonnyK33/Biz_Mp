e0ba0e734c9df6e0c173c5718cd94817b8903219
Sonny Kushwaha <Kushwaha@CompassRoseAM.com>
2021-09-19 22:03:13 -0400

    got working index route, hello world

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
-from flask import Flask, current_app
+from flask import Flask, config
 
-def create_app():
+
+
+def create_app(config_class=config):
     app = Flask(__name__)
+
+    app.config.from_object(config_class)
+    
+    #register blueprints:
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    
+    return app
