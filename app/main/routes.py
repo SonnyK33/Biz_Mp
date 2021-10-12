a14f3859a3f56a99de25d70c7b67b7dcb98ef65b
Sonny Kushwaha <Kushwaha@CompassRoseAM.com>
2021-10-11 21:15:38 -0400

    added profile pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,13 +14,12 @@ def index():
     
     return render_template('index.html',title="Home",Users=Users)
 
+@bp.route('/user/<username>')
+@login_required
+def user(username):
+    if current_user.is_authenticated:
+      
 
+        user = Users.query.filter_by(username=username).first_or_404()
+        return render_template('user.html',user=user)
 
-
-
-
-
-    # for database:
-    #     seller name, contact info, name of business, description, asking price
-    #      
-    
