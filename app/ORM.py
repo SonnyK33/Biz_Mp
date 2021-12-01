9e175e0c40daf58360c741fec75d398618a58ab7
Sonny Kushwaha <Kushwaha@CompassRoseAM.com>
2021-11-30 22:24:01 -0500

    continued work on domain model

---
+++ app/ORM.py
@@ -0,0 +1,1 @@
+"""TODO: map classes from domain_model to SQL tables"""
---
+++ app/repository.py
@@ -0,0 +1,3 @@
+"""TODO:
+following domain driven design's repo pattern
+should have functions to add, get, and remove items from database (another todo"""
